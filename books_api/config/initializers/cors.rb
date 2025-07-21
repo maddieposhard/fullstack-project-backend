4fad9828ca75f5ce53b25dadad5db807798e118f
maddieposhard <poshardmadeline@gmail.com>
2025-07-21 10:01:47 -0500

    updated the cors to get local host 4200 to work in angular

--- books_api/config/initializers/cors.rb
+++ books_api/config/initializers/cors.rb
@@ -14,3 +14,16 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
        methods: [:get, :post, :put, :patch, :delete, :options, :head]
    end
  end
+
+
+ Rails.application.config.middleware.insert_before 0, Rack::Cors do
+    allow do
+      origins 'http://localhost:4200'  # your Angular dev server
+  
+      resource '*',
+        headers: :any,
+        methods: [:get, :post, :put, :patch, :delete, :options, :head],
+        credentials: false
+    end
+  end
+  

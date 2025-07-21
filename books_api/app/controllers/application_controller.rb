8c993a1e1c0c34c9058bfb876542729877d9e721
maddieposhard <poshardmadeline@gmail.com>
2025-07-21 14:13:58 -0500

    updated before action so that login function works

--- books_api/app/controllers/application_controller.rb
+++ books_api/app/controllers/application_controller.rb
@@ -1,9 +1,4 @@
 class ApplicationController < ActionController::API
-    before_action :authenticate_request
-    attr_reader :current_user
-
-    private
-
     def authenticate_request
         header = request.headers['Authorization']
         header = header.split(' ').last if header
